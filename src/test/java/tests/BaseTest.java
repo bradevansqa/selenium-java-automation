4b57a156ac17a338aa66b44a2ba3345371c8cce2
Brad Evans <bradevansqa@gmail.com>
2025-10-06 11:28:30 -0400

    pom update

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,20 +1,32 @@
 package tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class BaseTest {
+
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void setUp() throws Exception {
+        ChromeOptions options = new ChromeOptions();
+
+        // ✅ Add flags for CI
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        // ✅ Use a unique temp directory for Chrome's user data
+        Path tempDir = Files.createTempDirectory("chrome-user-data");
+        options.addArguments("--user-data-dir=" + tempDir.toString());
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://www.bankofamerica.com/security-center/overview/");
     }
 
     @AfterMethod
